7c5acc8fa9beb723964b249409d9b12d01ecf08a
Sören Michaels <soeren.michaels@gmail.com>
2021-11-02 17:48:33 +0100

    added layout with NavBar

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,8 +1,10 @@
 <script language="ts">
 	import NavBar from '../components/NavBar.svelte';
 </script>
-
-<div>
+<div
+	class="mx-auto m-5 p-2 max-w-screen-lg rounded-xl
+	bg-blue-300 shadow-xl "
+>
 	<NavBar />
 	<slot />
 </div>
