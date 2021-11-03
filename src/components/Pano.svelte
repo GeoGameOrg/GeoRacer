13e473369ebee4ba2bc5bae23bed43bd00b75c66
Sören Michaels <soeren.michaels@gmail.com>
2021-11-03 22:00:31 +0100

    added map and pano components :)

---
+++ src/components/Pano.svelte
@@ -0,0 +1,84 @@
+<script lang="ts" context="module">
+	import { writable } from 'svelte/store';
+	class Pano {
+		private _loaded = false;
+		private _pano: null | google.maps.StreetViewPanorama = null;
+		private _items: google.maps.Marker[] = [];
+		get loaded() {
+			return this._loaded;
+		}
+
+		get pano () {return this._pano}
+		set pano(pano: google.maps.StreetViewPanorama) {
+			this._loaded = true;
+			this.resolvePromise();
+			this._pano = pano;
+		}
+		resolvePromise: Function;
+
+		wait = new Promise<void>((resolve) => {
+			this.resolvePromise = resolve;
+
+			if (this._loaded) {
+				resolve();
+			}
+		});
+
+		listenForPositionChange(handler) {
+			if (this._loaded) {
+				this._pano.addListener('position_changed', handler);
+			} else throw 'cannot add listener pano not loaded';
+		}
+
+		addItem({ position, icon, title }) {
+			if (this._loaded) {
+				this._items.push(
+					new google.maps.Marker({
+						position,
+						map: this._pano,
+						icon,
+						title
+					})
+				);
+			} else {
+				throw 'couldnt add items to Panorama bc its not loaded';
+			}
+		}
+
+		setPanoId(panoId: string) {
+			if (this._loaded) {
+				this._pano.setPano(panoId);
+			} else {
+				throw 'Panorama is not loaded';
+			}
+		}
+	}
+
+	export const pano = writable(new Pano());
+</script>
+
+<script lang="ts">
+	import { Loader } from '@googlemaps/js-api-loader';
+	const loader = new Loader({
+		apiKey: 'AIzaSyAD6HVJHgRC0i5nqcX7Pnu9veKbSRSN5C0'
+	});
+	export const position = { lat: 42.345573, lng: -71.098326 };
+	loader.load().then(() => {
+		$pano.pano = new google.maps.StreetViewPanorama(
+			document.getElementById('pano') as HTMLElement,
+			{
+				position,
+				disableDefaultUI: true,
+				pov: {
+					heading: 34,
+					pitch: 10
+				}
+			} as google.maps.MapOptions
+		);
+	});
+</script>
+
+<div id="pano" class="w-full h-screen rounded-xl shadow-xl" />
+
+<style>
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,25 @@
 <script language="ts">
+	import Map, { map } from '../components/Map.svelte';
+	import Pano, { pano } from '../components/Pano.svelte';
+	$pano.wait.then(() =>{
+		$pano.addItem({
+			position: { lat: 42.345573, lng: -71.098326 },
+			icon: null,
+			title: 'test'
+		})
+		$pano.listenForPositionChange(() =>console.log($pano.pano.getPosition()))
+		}
+	)
+	$map.wait.then(() =>
+		$map.addItem({
+			position: { lat: 42.345573, lng: -71.098326 },
+			icon: null,
+			title: 'test'
+		})
 
+	);
 </script>
+
 <div class="flex justify-center text-white"><span>Beta</span></div>
+<Map />
+<Pano />
