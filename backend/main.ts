9fe5e215b1b6e1f532b7aae46d3fd82e9bdb4c17
Noah Kamara <mail@noahkamara.com>
2021-11-04 00:12:49 +0100

    added endpoints: createRoom joinRoom id

--- backend/main.ts
+++ backend/main.ts
@@ -1,55 +1,111 @@
 import { Server } from 'socket.io';
 import { v4 as uuidv4 } from 'uuid'
+import { CreateRoomSocketResponse, JoinRoomSocketResponse, IdSocketResponse } from './models/socketResponse'
+import { Lobby, Lobbies } from './models/lobby'
+import { Player } from './models/player'
 
-// source: https://stackoverflow.com/a/2117523
-enum GamePhase {
-	Lobby,
-	Ingame,
-	End
-}
 declare module 'socket.io' {
 	interface Socket {
-		uuid: string;
+		uuid: string
+		username: string
 	}
 }
-interface Player {
-	username: string,
-	uuid: string,
-}
 
-interface Lobby {
-	players: Player[];
-	gamePhase: GamePhase;
-	settings: { [key: string]: any };
-}
 
-const defaultLobby: Lobby = {
-	players: [],
-	gamePhase: GamePhase.Lobby,
-	settings: {}
-};
-interface Lobbies {
-	[key: string]: Lobby
-}
 const lobbies: Lobbies = {};
 
 const io = new Server({ cors: { origin: 'http://localhost:3000', methods: ['GET', 'POST'] } });
 
 io.on('connection', (socket) => {
 	console.log(socket.id, 'connected');
-	socket.on('joinRoom', (room, callback) => {
-		console.log(socket.id, 'joined room: ', room);
-		let success = true;
-		callback(success)	
+	socket.on('createRoom', (callback: (response: CreateRoomSocketResponse) => void) => {
+		console.debug(socket.id, "[createRoom]");
+
+		// Check for socket uuid
+		if (socket.uuid !== undefined) {
+			console.debug(socket.id, "[createRoom]", `keeping id '${socket.uuid}'`);
+		} else {
+			socket.uuid = uuidv4();
+			console.debug(socket.id, "[createRoom]", `generated id '${socket.uuid}'`);
+		}
+
+		// Check for Username (Create if undefined)
+		var username = socket.username;
+		if (username === undefined) {
+			username = uuidv4()
+			console.debug(socket.id, "[createRoom]", `generated username '${username}'`);
+		} else {
+			console.debug(socket.id, "[createRoom]", `keeping username '${username}'`);
+		}
+
+		// Create & Save Player
+		let hostPlayer = new Player(username, socket.uuid)
+
+		// Create & Save Lobby
+		let lobbyId = uuidv4();
+		let lobby = Lobby.withHost(hostPlayer, lobbyId)
+		lobbies[lobbyId] = lobby
+		console.log("PLAYERS", lobby.players)
+		console.log(lobbies[lobbyId])
+
+		socket.join(lobbyId);
+		callback(CreateRoomSocketResponse.success(lobby))
 	});
-	socket.on('id', (id: null | string, callback) => {
+
+	socket.on('joinRoom', (lobbyId: string, callback: (response: JoinRoomSocketResponse) => void) => {
+		console.debug(socket.id, "[joinRoom]");
+		// Check if Lobby exists
+		if (lobbies[lobbyId] === undefined) {
+			console.error(socket.id, "[joinRoom]", "error: lobby not found");
+			callback(JoinRoomSocketResponse.failure(`Lobby '${lobbyId}' not found`));
+			return;
+		}
+
+		// Check for socket uuid
+		if (socket.uuid !== undefined) {
+			console.debug(socket.id, "[joinRoom]", `keeping id '${socket.uuid}'`);
+		} else {
+			socket.uuid = uuidv4();
+			console.debug(socket.id, "[joinRoom]", `generated id '${socket.uuid}'`);
+		}
+
+		// Check for Username (Create if undefined)
+		if (socket.username === undefined) {
+			socket.username = uuidv4()
+			console.debug(socket.id, "[joinRoom]", `generated username '${socket.username}'`);
+		} else {
+			console.debug(socket.id, "[joinRoom]", `keeping username '${socket.username}'`);
+		}
+
+		let player: Player = new Player(socket.username, socket.uuid)
+		if (!lobbies[lobbyId].players.some(p => p.uuid == player.uuid)) {
+			lobbies[lobbyId].players.push(player)
+		} else {
+			console.info(socket.id, "[joinRoom]", `failure: ${player.uuid} already in lobby`);
+			callback(JoinRoomSocketResponse.failure("Already in Lobby"))
+			return
+		}
+
+		socket.join(lobbyId);
+		console.info(socket.id, "[joinRoom]", "successful:" + player);
+		callback(JoinRoomSocketResponse.success(lobbies[lobbyId]))
+	});
+
+	socket.on('id', (id: null | string, callback: (response: IdSocketResponse) => void) => {
+		console.debug(socket.id, "[id]");
+
+		//TODO: Generate ID if a previous one was not provided
 		if (id === null) {
 			id = uuidv4();
+			console.debug(socket.id, "[id]", `success: generated '${id}'`);
+		} else {
+			console.debug(socket.id, "[id]", `success: provided '${id}'`);
 		}
-		socket['uuid'] = id;
 
-		console.log("added uuid to", socket.id);
-		callback(id);
+		socket['uuid'] = id;
+		console.info(socket.id, "[id]", `assigned id '${id}' to socket`);
+
+		callback(IdSocketResponse.success(id));
 	});
 
 });
---
+++ src/routes/debug.svelte
@@ -0,0 +1,42 @@
+<script language="ts">
+	import { createRoom, joinRoom } from '../helper.ts';
+
+	var responseTxt = '';
+
+    function parseResponse(response) {
+        responseTxt = JSON.stringify(response, null, 2)
+    }
+
+    // Bindings
+    let joinRoomId = '';
+</script>
+
+<div class="flex justify-center text-white"><span>Beta</span></div>
+<div class="text-white">
+	<pre class="text-white bg-black">
+        {responseTxt}
+    </pre>
+	<div>
+		<button on:click={() => createRoom(parseResponse)}> Create Room </button>
+        <div>
+            <button on:click={() => joinRoom(joinRoomId, parseResponse)}> Join Room </button>
+            <input bind:value={joinRoomId} placeholder="enter room id">
+        </div>
+	</div>
+</div>
+
+<style>
+	.button {
+		border: none;
+		color: white;
+        background-color: pink;
+        border-radius: 10px;
+		padding: 15px 32px;
+		text-align: center;
+		text-decoration: none;
+		display: inline-block;
+		font-size: 16px;
+		margin: 4px 2px;
+		cursor: pointer;
+	}
+</style>
