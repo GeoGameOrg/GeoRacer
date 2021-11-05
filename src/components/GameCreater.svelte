96c5291858f4f076d98c7ff438f338e56f7dd77b
Sören Michaels <soeren.michaels@gmail.com>
2021-11-05 16:26:15 +0100

    added gameCreator

---
+++ src/components/GameCreater.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+	import Map from './Map.svelte';
+	enum PointTypes {
+		checkpoint,
+		item,
+		start,
+		end
+	}
+	class Point {
+		marker: google.maps.Marker;
+		type: PointTypes;
+		index: number | null;
+	}
+	class Game {
+		title = '';
+		description = '';
+		points: Point[];
+    boundaries: google.maps.LatLngBounds | google.maps.LatLngBoundsLiteral
+	}
+	const gameForm = new Game();
+</script>
+
+<h1 class="text-white text-xl font-bold text-center">Create Game</h1>
+<form class="grid text-white m-5">
+	<label class="items-center">Game Title</label>
+	<input
+		type="text"
+		placeholder="Sightseeing London Ralley"
+		class="text-black shadow-md rounded-md m-5 p-2"
+		value={gameForm.title}
+    on:input={(e) => gameForm.title = e.target.value }
+	/>
+	<label>Description</label>
+	<input
+		type="text"
+		placeholder="most visited tourist locations in London"
+		class="text-black shadow-md rounded-md m-5 p-2"
+		value={gameForm.description}
+    on:input={(e) => gameForm.description = e.target.value }
+	/>
+	<button
+		on:click={(e) => {
+			e.preventDefault();
+			console.log(gameForm);
+		}}>log game</button
+	>
+</form>
+<Map />
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,8 @@
 <script language="ts">
+import GameCreater from "../components/GameCreater.svelte";
+
+
 </script>
 
 <div class="flex justify-center text-white"><span>Beta</span></div>
+<GameCreater/>
