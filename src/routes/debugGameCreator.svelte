ef550deecf2eeb29881aaf2e790c8d0168facffb
Sören Michaels <soeren.michaels@gmail.com>
2021-11-12 16:25:20 +0100

    new route to debug GameCreator

---
+++ src/routes/debugGameCreator.svelte
@@ -0,0 +1,7 @@
+<script>
+
+import GameCreator from "../components/GameCreator.svelte";
+
+</script>
+
+<GameCreator/>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,7 @@
 <script language="ts">
-import GameCreater from "../components/GameCreater.svelte";
 
 
 
 </script>
 
 <div class="flex justify-center text-white"><span>Beta</span></div>
-<GameCreater/>
